0ff96928958171dc16a0b40a7dfc7f22893b4d5d
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-08-12 00:42:46 +0000

    cambios

---
+++ src/config/constants.js
@@ -0,0 +1,14 @@
+export const CHOISES = {
+  ROCK: { id: 'ROCK', name: 'Piedra', icon:''},
+  PAPER: { id: 'PAPER', name: 'Papel', icon:''},
+  SCISSORS: { id: 'SCISSORS', name: 'Tijera', icon:''}  
+}
+
+export const SCREENS = {
+     START: 'START',
+     MENU: 'MENU',
+     GAME: 'GAME',
+     GAMEOVER: 'GAMEOVER',
+}
+    
+export const STORAGE_KEY = 'RPS_SAVED_GAME_STATE'
