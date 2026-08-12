acc061dbefd08a2e9db5b1ae44dc1ca0afa759cc
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-08-12 19:01:56 +0000

    agregando codigo a dom.js

---
+++ src/utils/dom.js
@@ -0,0 +1,14 @@
+export const createElements = (tag, attributes = {}, ...children) => {
+const element = document.createElement(tag)
+
+Object.entries(attributes).forEach([KeyboardEvent, value]) => {
+   if (KeyboardEvent,startsWith('on')) &&  typeof value === 'function' {
+     Element.addEventListener(KeyboardEvent.substring(2).toLowerCase(), value)
+}     else if  
+
+export const getRandomChoice = (choices) => {
+}
+
+export const evaluateRound = (playerChoice, cpuChoice) => {
+
+}
