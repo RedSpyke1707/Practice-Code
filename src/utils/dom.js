c488e5b648954981d699554c8d96a9840f8e0ac4
RedSpyke1206 <psgabriel.sp@gmail.com>
2026-08-13 04:07:07 +0000

    continuamos agregando codigo en dom.js

--- src/utils/dom.js
+++ src/utils/dom.js
@@ -1,10 +1,27 @@
 export const createElements = (tag, attributes = {}, ...children) => {
 const element = document.createElement(tag)
 
-Object.entries(attributes).forEach([KeyboardEvent, value]) => {
-   if (KeyboardEvent,startsWith('on')) &&  typeof value === 'function' {
-     Element.addEventListener(KeyboardEvent.substring(2).toLowerCase(), value)
-}     else if  
+Object.entries(attributes).forEach(([key, value]) => {
+if (key.startsWith('on') && typeof value === 'function') {
+ element.addEventListener(key.substring(2).toLowerCase(), value)
+} else if (key === 'className') {
+  element.className = value
+} else {
+  element.setAttribute(key, value)
+}
+})    
+
+children.flat(Infinity).forEach(child => {
+if (child === null || child === undefined || child === false) return
+if (typeof child === 'string' || typeof child === 'number') {
+  element.appendchild(document.createTextNode(child))
+} else if (child instanceof HTMLElement) {
+  element,appendchild(child)
+  }
+}) 
+
+return element
+}
 
 export const getRandomChoice = (choices) => {
 }
